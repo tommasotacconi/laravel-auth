76de0ab28f699a558ae6653fece64ee0b6c356b7
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-05 16:13:22 +0100

    corrected create_projects_table migration, filled seeder run function and created fillable, added seeder to call function in DatabaseSeeder

--- database/migrations/2024_11_04_175710_create_table_projects.php
+++ database/migrations/2024_11_04_175710_create_table_projects.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30);
-            $table->json('authors', 255);
+            $table->string('authors', 255);
             $table->tinyText('arguments');
-            $table->string('programming_laguages', 150);
+            $table->string('programming_languages', 150);
             $table->date('start_date')->nullable(true);
             $table->date('end_date')->nullable(true);
             $table->timestamps();
