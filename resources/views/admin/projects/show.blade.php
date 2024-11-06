2a7c8e4c762648cf5c068f1e9297be6da139f562
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-06 17:29:28 +0100

    add views index, show and create

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+	<h1 class="ms-1">Admin/index</h1>
+
+	<h1 class="text-center p-3">
+		{{ $project->name }}
+    </h1>
+    <div class="container-lg">
+        <div class="project-property">
+            <b>Authors</b>: {{ $project->authors }}
+        </div>
+        <div class="project-property">
+            <b>Arguments</b>
+            <p>{{ $project->arguments }}</p>
+        </div>
+        <div class="project-property">
+            <b>Linguaggi di programmazione</b>: {{ $project->programming_languages }}
+        </div>
+        <div class="project-property">
+            <b>Data d'inizio</b>: {{ $project->start_date }}, <b>data di fine</b>: {{ $project->end_date }}
+        </div>
+    </div>
+@endsection
