7f5579ac74e0ca53463a7a8e07e35717d7d82bc5
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-06 17:30:48 +0100

    reviewed layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <a href="{{ route('admin.projects.create') }}">
+                    <i class="fa-solid fa-plus"></i>
+                </a>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
